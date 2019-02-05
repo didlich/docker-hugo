7043fdec23c2e925873a0fe88911c59e921626e2
didlich <didlich@t-online.de>
2019-02-05 15:54:36 +0100

    optimize UID and GID modification

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,21 +5,19 @@ set -e
 echo "USER_GID: "${USER_GID:-9001}
 echo "USER_UID: "${USER_UID:-9001}
 
-## Change GID for USER?
-if [ -n "${USER_GID}" ] && [ "${USER_GID}" != "`id -g ${HUGO_USER}`" ]; then
-    sed -i -e "s/^${HUGO_USER}:\([^:]*\):[0-9]*/${HUGO_USER}:\1:${USER_GID}/" /etc/group
-    sed -i -e "s/^${HUGO_USER}:\([^:]*\):\([0-9]*\):[0-9]*/${HUGO_USER}:\1:\2:${USER_GID}/" /etc/passwd
-    #chown -R --dereference -L ${HUGO_USER}:${HUGO_GROUP} ${HUGO_HOME}
-fi
-
-## Change UID for USER?
-if [ -n "${USER_UID}" ] && [ "${USER_UID}" != "`id -u ${HUGO_USER}`" ]; then
-    sed -i -e "s/^${HUGO_USER}:\([^:]*\):[0-9]*:\([0-9]*\)/${HUGO_USER}:\1:${USER_UID}:\2/" /etc/passwd
-    #chown -R --dereference -L ${HUGO_USER}:${HUGO_GROUP} ${HUGO_HOME}
+## Change UID and GID for USER?
+if [ -n "${USER_UID}" ]; then
+    if [ -n "${USER_GID}" ]; then
+        echo "-> change USER_UID and USER_GID"
+        sed -i -e "s/^${HUGO_USER}:\([^:]*\):[0-9]*/${HUGO_USER}:\1:${USER_GID}/" /etc/group
+        sed -i -e "s/^${HUGO_USER}:\([^:]*\):\([0-9]*\):[0-9]*/${HUGO_USER}:\1:\2:${USER_GID}/" /etc/passwd    
+        sed -i -e "s/^${HUGO_USER}:\([^:]*\):[0-9]*:\([0-9]*\)/${HUGO_USER}:\1:${USER_UID}:\2/" /etc/passwd
+        #chown -R --dereference -L ${HUGO_USER}:${HUGO_GROUP} ${HUGO_HOME}
+    fi
 fi
 
 HUGO=/usr/local/sbin/hugo
 echo "Hugo path: $HUGO"
-echo "ARGS:      $@" 
+echo "ARGS:      $@"
 
 su-exec ${HUGO_USER} $@
